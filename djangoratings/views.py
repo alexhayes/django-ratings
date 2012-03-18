517c9091d1fab635c32dce2397ec338790281d6a
Alex Hayes <alex@alution.com>
2012-03-19 08:55:52 +1100

    Using ugettext rather than ugettext_lazy for views as 'sequence item 0: expected string, __proxy__ found' was occuring when calling HttpResponse

--- djangoratings/views.py
+++ djangoratings/views.py
@@ -1,6 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.http import HttpResponse, Http404
 
 from exceptions import *
